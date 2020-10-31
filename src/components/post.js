44cb7d57b619ce3f4f0c32e37236be2fb91b461f
Chris Hayes <chrisroberthayes@gmail.com>
2020-10-31 13:42:38 -0400

    Initial work on article page

--- src/components/post.js
+++ src/components/post.js
@@ -1,9 +1,26 @@
+/** @jsx jsx */
 import React from "react"
+import Image from "gatsby-image"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { graphql, useStaticQuery } from "gatsby"
+import { jsx, Container, Grid, Styled } from "theme-ui"
 
 import SEO from "./seo"
+import Tags from "./tags"
+import Layout from "./layout"
 
-const Post = ({ data: { blogPost: post } }) => (
-  <>
+const Post = ({
+  data: {
+    blogPost: post,
+    site: {
+      siteMetadata: { title },
+    },
+  },
+  location,
+  previous,
+  next,
+}) => (
+  <Layout location={location} title={title}>
     <SEO
       title={post.title}
       description={post.excerpt}
@@ -13,8 +30,82 @@ const Post = ({ data: { blogPost: post } }) => (
       }
       imageAlt={post.imageAlt}
     />
-    Post
-  </>
+    <Container as="main" sx={{ maxWidth: "blogPostContainer" }}>
+      <article sx={{ fontSize: 3 }}>
+        <header sx={{ mt: 7 }}>
+          <Tags tags={post.tags} />
+          <Styled.h1 sx={{ mt: 2 }}>{post.title}</Styled.h1>
+          <Meta date={post.date} />
+        </header>
+        <section>
+          <MDXRenderer>{post.body}</MDXRenderer>
+        </section>
+      </article>
+    </Container>
+  </Layout>
 )
+
+const Meta = ({ date }) => {
+  const data = useStaticQuery(graphql`
+    query PostMetaQuery {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+      avatar: file(
+        absolutePath: { regex: "assets/avatar.(jpeg|jpg|gif|png)/" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 50, maxHeight: 50) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  const {
+    site: {
+      siteMetadata: { author },
+    },
+    avatar,
+  } = data
+
+  return (
+    <Grid
+      columns="min-content auto"
+      sx={{
+        gridGap: 3,
+        alignItems: "center",
+        mt: 3,
+        mb: 6,
+      }}
+    >
+      <Image
+        sx={{
+          width: 50,
+          borderRadius: "50%",
+        }}
+        fluid={avatar.childImageSharp.fluid}
+        alt={author}
+      />
+      <div>
+        <span rel="author" sx={{ fontSize: 2 }}>
+          {author}
+        </span>
+        <time
+          sx={{
+            display: "block",
+            fontSize: 1,
+            letterSpacing: "loose",
+            color: "muted",
+          }}
+        >
+          {date}
+        </time>
+      </div>
+    </Grid>
+  )
+}
 
 export default Post
